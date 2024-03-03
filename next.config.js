bccd837f4aed914d4e04d0ec398cc17b2ad855c6
termat <ter.matsuoka@gmail.com>
2024-03-03 22:06:33 +0900

    新規作成

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-/*
 const nextConfig = {}
-module.exports = nextConfig
-*/
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "export",
-  }
-  
-  module.exports = nextConfig
+module.exports = nextConfig

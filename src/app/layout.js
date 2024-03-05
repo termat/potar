a37d83034d07568f9699f6de5e3749441e11aee9
termat <ter.matsuoka@gmail.com>
2024-03-05 22:54:37 +0900

    更新

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
-import Head from "next/head";
 
 const inter = Inter({ subsets: ['latin'] })
-
-export default function Meta() {
-  return (
-    <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-   </Head>
-  )
-}
 
 export const metadata = {
   title: 'Potar',

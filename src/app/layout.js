2531cb48badb3223aede54390e4ee9294ba3c876
termat <ter.matsuoka@gmail.com>
2024-03-05 22:49:41 +0900

    更新

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,25 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Head from "next/head";
 
 const inter = Inter({ subsets: ['latin'] })
+
+export default function Meta() {
+  return (
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+   </Head>
+  )
+}
 
 export const metadata = {
   title: 'Potar',
   description: 'ポタリングの記録',
   viewport:'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0',
-  'twitter:card':'summary_large_image',
-  'twitter:site':'@t_mat',
+  twitter:{
+    card:'summary_large_image',
+    site:'@t_mat'
+  },
   openGraph: {
     url: 'https://termat.github.io/potar/',
     title: 'potar：ポタリングの記録',

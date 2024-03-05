603f84e2b7c6274fe0c6b5a9fb12e4a15fd184fe
termat <ter.matsuoka@gmail.com>
2024-03-05 22:33:54 +0900

    更新

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,15 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Potar',
   description: 'ポタリングの記録',
+  viewport:'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0',
+  'twitter:card':'summary_large_image',
+  'twitter:site':'@t_mat',
+  openGraph: {
+    url: 'https://termat.github.io/potar/',
+    title: 'potar：ポタリングの記録',
+    description:'ポタリングした地域を3D地図上で俯瞰するWebアプリの実験サイトです。',
+    image:'https://termat.github.io/potar/icons/back2.jpg'
+  },
 }
 
 export default function RootLayout({ children }) {
